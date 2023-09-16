7c618a43073ff32f43b10163709742bf909ab3a2
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-09-16 17:34:59 -0600

    finish counter app

--- projects/counterApp/index.js
+++ projects/counterApp/index.js
@@ -1,1 +1,14 @@
-// javascript
+let count = 0;
+let saveP = document.getElementById('save-el');
+
+function increment() {
+    count++;
+    document.getElementById('count-el').innerText = count;
+}
+
+function save() {
+    let countDash = count + ' - ';
+    saveP.textContent += countDash;
+    count = 0;
+    document.getElementById('count-el').innerText = count;
+}
