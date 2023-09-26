d9c1a215f61b90a09031492ee8fc212e0c3b0b7f
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-09-26 00:06:55 -0400

    got inputs from page to js

--- projects/04paintCarpCalc/js/scripts.js
+++ projects/04paintCarpCalc/js/scripts.js
@@ -1,1 +1,16 @@
-console.log("I am connected");
+let x = new Date();
+let year = x.getFullYear();
+let yearSpan = document.querySelector('#year');
+yearSpan.textContent = year;
+
+
+let button = document.querySelector('#calculate');
+button.addEventListener('click', () => {
+    const width = Number(document.querySelector('#width').value);
+    const depth = Number(document.querySelector('#depth').value);
+    const height = Number(document.querySelector('#height').value);
+    const paintType = document.querySelector('#quality').selectedOptions[0].text;
+    const quality = Number(document.querySelector('#quality').selectedOptions[0].value);
+
+
+}); // end of the calculate function 
