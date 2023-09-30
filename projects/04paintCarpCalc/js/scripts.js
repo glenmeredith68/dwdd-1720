8c9b3f4850a4972b69e24030b6e77b5bce19902f
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-09-29 22:07:52 -0400

    addListItems function done

--- projects/04paintCarpCalc/js/scripts.js
+++ projects/04paintCarpCalc/js/scripts.js
@@ -3,6 +3,13 @@ let year = x.getFullYear();
 let yearSpan = document.querySelector("#year");
 yearSpan.textContent = year;
 
+let addListItems = (listId, message) => {
+  let addingList = document.querySelector(`#${listId}`);
+  let liToAdd = document.createElement('li');
+  liToAdd.textContent = message;
+  addingList.appendChild(liToAdd);
+};
+
 let button = document.querySelector("#calculate");
 button.addEventListener("click", () => {
   const width = Number(document.querySelector("#width").value);
@@ -13,50 +20,43 @@ button.addEventListener("click", () => {
     document.querySelector("#quality").selectedOptions[0].value
   );
 
-// logs
-console.log('width = ' + width);
-console.log('height = ' + height);
-console.log('depth = ' + depth);
-console.log('quality = ' + quality);
+  let carpet = Math.ceil((width * depth) / 9);
+  carpet += " yards of carpet";
+  addListItems('carpet', carpet);
 
-
-
-let carpet = Math.ceil((width * depth) / 9);
-carpet += " yards of carpet";
-console.log('carp = ' + carpet);
-
-let tack = width * 2 + depth * 2;
-tack += " feet of tackstrip";
-console.log('tack = ' + tack);
+  let tack = width * 2 + depth * 2;
+  tack += " feet of tackstrip";
+  addListItems('carpet', tack);
 
   let walls = width * height * 2 + depth * height * 2;
-  console.log(walls);
   let gallons = Math.ceil(walls / quality);
+  let gallonsValue = gallons;
   gallons += ` gallons of ${paintType} paint for the walls`;
-  console.log(gallons);
+  addListItems('paint', gallons);
 
-  let ceiling = Math.ceil(width * depth / 400);
-  console.log('ceiling needs ' + ceiling);
+  let ceiling = Math.ceil((width * depth) / 400);
+  let primer =  ceiling + gallonsValue;
+  ceiling += ' gallons of flat paint for the ceiling'
+  addListItems('paint', ceiling);
+  
+  
+  primer += ' gallons of primer';
+  addListItems('paint', primer);
+  
+  // adding to list now
+//   let carpList = document.querySelector("#carpet");
+//   let tackLi = document.createElement("li");
+//   let carpetLi = document.createElement("li");
+//   carpetLi.textContent = carpet;
+//   tackLi.textContent = tack;
+//   carpList.appendChild(carpetLi);
+//   carpList.appendChild(tackLi);
 
-  let primer = ceiling + walls;
-
-// adding to list now
-let carpList = document.querySelector('#carpet');
-let tackLi = document.createElement('li');
-let carpetLi = document.createElement('li');
-carpetLi.textContent = carpet;
-tackLi.textContent = tack;
-carpList.appendChild(carpetLi);
-carpList.appendChild(tackLi);
-
-let paintList = document.querySelector('#paint');
-let primerLi = document.createElement('li');
-let gallonsLi = document.createElement('li');
-primerLi.textContent = primer;
-gallonsLi.textContent = gallons;
-paintList.appendChild(primerLi);
-paintList.appendChild(gallonsLi);
-
-
-
+//   let paintList = document.querySelector("#paint");
+//   let primerLi = document.createElement("li");
+//   let gallonsLi = document.createElement("li");
+//   primerLi.textContent = primer;
+//   gallonsLi.textContent = gallons;
+//   paintList.appendChild(primerLi);
+//   paintList.appendChild(gallonsLi);
 }); // end of the calculate function
