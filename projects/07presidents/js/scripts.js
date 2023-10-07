12bf30a1e63af0b020975c495f01a1cc390c4889
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-10-06 20:58:53 -0600

    finished stooges

--- projects/07presidents/js/scripts.js
+++ projects/07presidents/js/scripts.js
@@ -1,1 +1,22 @@
-console.log("I am connected");
+const stoogeData = [{
+    "name": "Curly",
+    "born": 1903
+},
+{
+    "name":"Moe",
+    "born":"1897"
+}]
+
+
+const cardPointer = document.querySelector('#cards');
+
+let theName = document.createElement('h2');
+let theBirth = document.createElement('p');
+
+theName.textContent = stoogeData[0].name;
+theBirth.textContent = stoogeData[0].born;
+
+cardPointer.appendChild(theName);
+cardPointer.appendChild(theBirth);
+
+function~
