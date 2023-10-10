019ab2dbc602aeffdbae09701ced096f4e72333a
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-10-10 13:29:11 -0600

    added base html

--- projects/08posters/js/scripts.js
+++ projects/08posters/js/scripts.js
@@ -1,1 +1,10 @@
-console.log("I am connected");
+
+let x = new Date();
+let year = x.getFullYear();
+let yearSpan = document.querySelector("#year");
+yearSpan.textContent = year;
+
+
+
+const swPostersLink = 'https://starwars-visual-guide.com/assets/img/films';
+// add a number.jpeg 
