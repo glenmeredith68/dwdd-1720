f7d51b1a5301d3cdd635bb0422061e44f67cb609
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-11-30 16:00:41 -0700

    images work

--- projects/09people/js/scripts.js
+++ projects/09people/js/scripts.js
@@ -1,1 +1,32 @@
-console.log("I am connected");
+import { people } from './people.js';
+
+// footer functionality 
+let x = new Date();
+let year = x.getFullYear();
+let yearSpan = document.querySelector("#year");
+yearSpan.textContent = year;
+
+const nav = document.querySelector('nav');
+const cardsHere = document.querySelector('.cards-here');
+
+// loop through the people
+function displayPeople(x) {
+x.forEach( person => {
+    const card = document.createElement('section');
+    const img = document.createElement('img');
+    const urlArray = person.url.split('/');
+    img.src = `https://starwars-visualguide.com/assets/img/characters/${urlArray[5]}.jpg`
+
+
+    const caption = document.createElement('figcaption');
+    caption.textContent = person.name;
+
+    // assemble parts 
+    card.appendChild(img);
+    card.appendChild(caption);
+    cardsHere.appendChild(card);
+} // end of fat arrow
+) //end of the loop
+
+} // end of function
+displayPeople(people);
