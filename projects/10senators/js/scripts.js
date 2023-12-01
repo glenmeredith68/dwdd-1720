c8153bcb868bd9a37f85bd936edbdc91e658f929
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-12-01 14:34:36 -0700

    image sizes working

--- projects/10senators/js/scripts.js
+++ projects/10senators/js/scripts.js
@@ -13,22 +13,27 @@ let medium = document.querySelector('.medium');
 let large = document.querySelector('.large');
 small.addEventListener('click', () => {
     imgSize = '-50px.jpeg';
+    console.log(imgSize);
     small.classList.add('selected');
     medium.classList.remove('selected');
     large.classList.remove('selected');
+    filterCards(selectedVal, imgSize);
     
 })
 medium.addEventListener('click', () => {
     imgSize = '-100px.jpeg';
+    console.log(imgSize);
     medium.classList.add('selected');
     small.classList.remove('selected');
     large.classList.remove('selected');
+    filterCards(selectedVal, imgSize);
 })
 large.addEventListener('click', () => {
     imgSize = '-200px.jpeg';
     large.classList.add('selected');
     small.classList.remove('selected');
     medium.classList.remove('selected');
+    filterCards(selectedVal, imgSize);
 })
 
 
@@ -36,33 +41,33 @@ large.addEventListener('click', () => {
 const linkBase = 'https://www.govtrack.us/static/legislator-photos/'
 const main = document.querySelector('main');
 
-function filterCards(query) {
+function filterCards(query, imgSize) {
+    main.textContent = '';
     for (let i = 0; i < senators.length; i++) {
         console.log(query);
-        if (query = 'all') {
-            makeCard(senators[i]);
-        } else if (query = 'women') {
-            if (senators[i].gender = 'F') {
-                makeCard(senators[i]);
+        if (query == 'all') {
+            makeCard(senators[i], imgSize);
+        } else if (query == 'women') {
+            if (senators[i].gender == 'F') {
+                makeCard(senators[i], imgSize);
             }
-        } else if (query = 'men') {
-            if (senators[i].gender = 'M') {
-                makeCard(senators[i]);
+        } else if (query == 'men') {
+            if (senators[i].gender == 'M') {
+                makeCard(senators[i], imgSize);
             }
-        } else if ((query = 'republican') && (senators[i].party = 'R')) {
-            makeCard(senators[i]);
-        } else if ((query = 'democrat') && (senators[i].party = 'D')) {
-            makeCard(senators[i]);
+        } else if ((query == 'republican') && (senators[i].party == 'R')) {
+            makeCard(senators[i], imgSize);
+        } else if ((query == 'democrat') && (senators[i].party == 'D')) {
+            makeCard(senators[i], imgSize);
         }
         else {
-            makeCard(senators[i]);
+            makeCard(senators[i], imgSize);
         }
     }
 }
-filterCards('all');
+filterCards('all', imgSize);
 
-function makeCard(senator) {
-    console.log(senator.party);
+function makeCard(senator, imgSize) {
     // create html elements
     let card = document.createElement('section');
     let h2 = document.createElement('h2');
@@ -81,6 +86,7 @@ function makeCard(senator) {
     // fill elements
     h2.textContent = senator.first_name + ' ' + senator.last_name;
     img.src = linkBase + senator.govtrack_id + imgSize;
+    console.log(img.src);
     descrip.textContent = senator.party + ' from ' + senator.state;
     phone.href = `tel: ${senator.phone}`;
     phone.textContent = senator.phone;
@@ -95,10 +101,17 @@ function makeCard(senator) {
 
 // add filter button functionality
 let allBtn = document.querySelector('.all-senators');
+let allVal = 'all';
 let womenBtn = document.querySelector('.women');
+let womenVal = 'women';
 let menBtn = document.querySelector('.men');
+let menVal = 'men';
 let republicanBtn = document.querySelector('.republican');
+let repVal = 'republican';
 let democratBtn = document.querySelector('.democrat');
+let demVal = 'democrat';
+
+let selectedVal;
 
 allBtn.addEventListener('click', () => {
     allBtn.classList.add('selected')
@@ -106,7 +119,8 @@ allBtn.addEventListener('click', () => {
     menBtn.classList.remove('selected')
     republicanBtn.classList.remove('selected')
     democratBtn.classList.remove('selected')
-    filterCards('all');
+    selectedVal = allVal;
+    filterCards(allVal, imgSize);
 })
 womenBtn.addEventListener('click', () => {
     allBtn.classList.remove('selected')
@@ -114,7 +128,8 @@ womenBtn.addEventListener('click', () => {
     menBtn.classList.remove('selected')
     republicanBtn.classList.remove('selected')
     democratBtn.classList.remove('selected')
-    filterCards('women');
+    selectedVal = womenVal;
+    filterCards('women', imgSize);
 
 })
 menBtn.addEventListener('click', () => {
@@ -123,7 +138,7 @@ menBtn.addEventListener('click', () => {
     menBtn.classList.add('selected')
     republicanBtn.classList.remove('selected')
     democratBtn.classList.remove('selected')
-    filterCards('men');
+    filterCards('men', imgSize);
 })
 republicanBtn.addEventListener('click', () => {
     allBtn.classList.remove('selected')
@@ -131,7 +146,7 @@ republicanBtn.addEventListener('click', () => {
     menBtn.classList.remove('selected')
     republicanBtn.classList.add('selected')
     democratBtn.classList.remove('selected')
-    filterCards('republican');
+    filterCards('republican', imgSize);
 })
 democratBtn.addEventListener('click', () => {
     allBtn.classList.remove('selected')
@@ -139,6 +154,5 @@ democratBtn.addEventListener('click', () => {
     menBtn.classList.remove('selected')
     republicanBtn.classList.remove('selected')
     democratBtn.classList.add('selected')
-    filterCards('democrat');
-    console.log('demo click');
+    filterCards('democrat', imgSize);
 })
