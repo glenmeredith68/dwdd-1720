c5a1f5c2698d949cff99111885ede8c2cb3a4248
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-12-02 14:58:03 -0700

    api connected

--- projects/12weather/js/scripts.js
+++ projects/12weather/js/scripts.js
@@ -1,4 +1,3 @@
-const apiKey = "33e2cecc8554b0c274a39f67cb50d0db";
 
 
 // footer functionality 
@@ -6,3 +5,29 @@ let x = new Date();
 let year = x.getFullYear();
 let yearSpan = document.querySelector("#year");
 yearSpan.textContent = year;
+
+
+// write a default zip 
+let zip = localStorage.getItem('zip');
+if (zip == null) {
+    let defaultZip = '02053';
+    localStorage.setItem('zip', defaultZip);
+    zip = defaultZip;
+}
+
+
+// setting path to weather api
+const apiKey = '5e02bd1d6d87a17f83809119a1f9fecb';
+const apiLink = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apiKey}&units=imperial`;
+
+// fetch remote json
+fetch(apiLink)
+    .then((response) => response.json())
+    .then((allData) => {
+        console.log(allData);
+    })
+
+// ask for new zipcode
+
+
+// data validate zipcode
