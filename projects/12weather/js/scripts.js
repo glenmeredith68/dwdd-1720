bd15b7c5fa95f39320cb2f4640bd6f595558dca5
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-12-02 16:52:37 -0700

    started styling

--- projects/12weather/js/scripts.js
+++ projects/12weather/js/scripts.js
@@ -1,10 +1,13 @@
-
-
 // footer functionality 
 let x = new Date();
 let year = x.getFullYear();
 let yearSpan = document.querySelector("#year");
 yearSpan.textContent = year;
+
+
+// references ot the html
+const weatherCard = document.querySelector('#weather-card');
+
 
 
 // write a default zip 
@@ -24,10 +27,60 @@ const apiLink = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&a
 fetch(apiLink)
     .then((response) => response.json())
     .then((allData) => {
-        console.log(allData);
+        displayWeather(allData);
     })
 
+
+// function that displays weather 
+function displayWeather(weatherData){
+    console.log(weatherData);
+    // add town name
+    const town = document.querySelector('#town');
+    town.textContent = 'Weather for ' + weatherData.name;
+
+    // current temp
+    const temp = document.createElement('p');
+    temp.className = 'temp';
+    temp.textContent = weatherData.main.temp + '°';
+    weatherCard.appendChild(temp);
+
+
+    // date 
+    const date = document.createElement('p');
+    date.className = 'date';
+    const d = new Date();
+    date.textContent = d.toDateString();
+    weatherCard.appendChild(date);
+
+    // icon
+    const weatherIcon = document.createElement('img');
+    weatherIcon.src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
+    weatherIcon.alt = weatherData.weather[0].description
+    weatherCard.appendChild(weatherIcon);
+}
+
 // ask for new zipcode
+const aside = document.querySelector('aside');
+const cog = document.querySelector('#cog');
+cog.addEventListener('click', () => {
+    aside.classList.toggle('show');
+})
+
+// set new zip
+const zipButton = document.querySelector('#apply-zip');
+zipButton.addEventListener('click', () => {
+    aside.classList.toggle('show');
+    let newZip = document.querySelector('#new-zip').value;
+    if (newZip.length == 5){
+    localStorage.setItem('zip', newZip);
+    } else
+    window.location.reload();
+})
+
 
 
 // data validate zipcode
+let input = document.querySelector('input');
+input.addEventListener('input', () => {
+    input.value = input.value.slice(0, 5);
+})
