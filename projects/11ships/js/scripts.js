c281f42b05d4e25deeae635f727a20ffb5178d79
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-12-01 22:57:52 -0700

    11 done

--- projects/11ships/js/scripts.js
+++ projects/11ships/js/scripts.js
@@ -1,1 +1,54 @@
-console.log("I am connected");
+const nav = document.querySelector("nav");
+const main = document.querySelector("main");
+
+fetch("js/starships.json")
+  .then((response) => response.json())
+  .then((shipArray) => {
+    populateNav(shipArray);
+  });
+
+//populate nav
+function populateNav(allShips) {
+  allShips.forEach((ship) => {
+    let btn = document.createElement("button");
+    btn.textContent = ship.name;
+    btn.addEventListener("click", () => {
+        showShip(ship);
+        let buttons = Array.from(nav.children);
+        buttons.forEach(button => button.classList.remove('selected'));
+        btn.classList.add('selected');
+
+    }); // end of event listener
+    nav.appendChild(btn);
+  });
+} // end of nav function
+
+// ship viewer
+function showShip(shipData) {
+  // create image etc
+  let section = document.createElement("section");
+  let img = document.createElement("img");
+  let h3 = document.createElement("h3");
+
+  // data to image
+  let urlArray = shipData.url.split("/");
+
+  img.src = `https://www.starwars-visualguide.com/assets/img/starships/${urlArray[5]}.jpg`;
+  img.alt = shipData.name;
+  h3.textContent = shipData.name;
+
+    // error checking for image
+    img.addEventListener('error', () => {
+        img.src = 'https://starwars-visualguide.com/assets/img/big-placeholder.jpg';
+        h3.textContent = `The ${shipData.name} is in dock for repairs.`;
+        img.alt = `${ship.name} image not found.`;
+    })
+
+  // add image to page
+  section.appendChild(img);
+  section.appendChild(h3);
+  main.textContent = "";
+  main.appendChild(section);
+}
+
+
