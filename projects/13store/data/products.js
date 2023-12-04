bfbef9f5076f326402b72eae126dcf95ebd99736
glenmeredith68 <glen.lee.meredith@gmail.com>
2023-12-03 23:16:55 -0700

    starting css

---
+++ projects/13store/data/products.js
@@ -0,0 +1,47 @@
+export const products = [
+  {
+    "name": "Magestic Eagle",
+    "photo": "eagle.jpg",
+    "dimensions": [
+      '5" x 8"',
+      '10" x 16"'
+    ],
+    "finish": 'Glossy'
+  },
+  {
+    "name": "Joyous Wedding",
+    "photo": "wedding.jpg",
+    "dimensions": [
+      '36" x 36"',
+      '100" x 100"'
+    ],
+    "finish": 'Matte'
+  },
+  {
+    "name": "Old Glory",
+    "photo": "flag.jpg",
+    "dimensions": [
+      '34" x 72"',
+      '5" x 8"'
+    ],
+    "finish": 'Flat'
+  },
+  {
+    "name": "Senior Portrait",
+    "photo": "senior.jpg",
+    "dimensions": [
+      '10" x 16"',
+      '3" x 2.5"'
+    ],
+    "finish": 'Glossy'
+  },
+  {
+    "name": "Play Ball",
+    "photo": "baseball.jpg",
+    "dimensions": [
+      '5" x 40"',
+      '10" x 80"'
+    ],
+    "finish": 'Steel'
+  }
+]
---
+++ projects/13store/js/scripts.js
@@ -0,0 +1,53 @@
+import {products} from '../data/products.js';
+
+// footer functionality 
+let x = new Date();
+let year = x.getFullYear();
+let yearSpan = document.querySelector("#year");
+yearSpan.textContent = year;
+
+// html references 
+const nav = document.querySelector('nav');
+const viewer = document.querySelector('.viewer');
+
+// months
+const months = ['January', 'February', 'March','April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+// nav items 
+products.forEach(product => {
+    const button = document.createElement('button');
+    button.textContent = product.name;
+    button.addEventListener('click', () => showProduct(product));
+
+    nav.appendChild(button);
+})
+
+function showProduct(product) {
+    let section = document.createElement('section');
+    let h2 = document.createElement('h2');
+    let img = document.createElement('img');
+    let finish = document.createElement('p');
+    let dimensions = document.createElement('ul');
+
+    h2.textContent = product.name;
+    img.src = `images/${product.photo}`;
+    img.alt = product.name;
+    finish.textContent = `Finish: ${product.finish}`;
+    dimensions.textContent = 'Dimensions';
+
+    product.dimensions.forEach(dimension => {
+        let li = document.createElement('li');
+        li.textContent = dimension;
+        dimensions.appendChild(li);
+    })
+
+    // assemble card
+    section.appendChild(h2);
+    section.appendChild(img);
+    section.appendChild(dimensions);
+    section.appendChild(finish);
+    viewer.textContent = '';
+    viewer.appendChild(section);
+    
+}
+showProduct(products[0]);
